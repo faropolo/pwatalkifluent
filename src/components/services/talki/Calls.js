bc11a663c1b24db08de2d786f00828f0caa15801
fabio.lopes <fabio.lopes@smiles.com.br>
2018-10-22 21:21:04 -0300

    Timer GetDados

--- src/components/services/talki/Calls.js
+++ src/components/services/talki/Calls.js
@@ -4,8 +4,13 @@ function getWaitingTalki() {
     let apiName = 'Talki';
     let path = '/call/talki'; 
     let myInit = { 
-        headers: {},
-        response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+        headers: {   
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Methods, Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT"
+        },
+        response: false, // OPTIONAL (return the entire Axios response object instead of only response.data)
         queryStringParameters: {  
             userID: '22222'
         }
